edfb73f90fc160b8e2e4e842324d0568adce43f4
Marileen Stamer <marileen@gmx.net>
2021-06-09 13:30:38 +0200

    added an urls-py file in the main application to map the view(s) as explained in this tutorial (ca. min 12 - 15) : https://www.youtube.com/watch?v=sm1mokevMWk

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+# define our paths to different web pages
+# urls for the different views in views.py file
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index(), name="index"),
+]
