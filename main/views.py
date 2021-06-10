26f15f7371ca5f37631eb58e18f4d187b15d57b7
Marileen Stamer <marileen@gmx.net>
2021-06-10 16:06:35 +0200

    pass the id from request to database and use it to display the name of the obejct --> explained in this tutorial (ca. min 40 - 42) : https://www.youtube.com/watch?v=sm1mokevMWk

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import School, Grade
 
 # Create your views here.
 
 
 def index(response,id):
-    return HttpResponse("<h1>erster test: %s</h1>" % id)
+    school = School.objects.get(id=id)
+    return HttpResponse("<h1>erster test: %s</h1>" % school.name)
