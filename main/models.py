e6c7f6e4ae08cae5255a635e0b0769d4b5971733
Marileen Stamer <marileen@gmx.net>
2021-06-10 14:06:09 +0200

    created models for school and class/grade as explained in this tutorial (ca. min 25 - 30) : https://www.youtube.com/watch?v=sm1mokevMWk

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=4)
