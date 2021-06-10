279e1fd8110b18c282d2523f7f737c3188aafe43
Marileen Stamer <marileen@gmx.net>
2021-06-10 14:28:12 +0200

    rum command 'python manage.py makemigrations main' to save the changes made on models AND ran 'python manage.py migrate' --> explained in this tutorial (ca. min 31 - 33) : https://www.youtube.com/watch?v=sm1mokevMWk

--- main/models.py
+++ main/models.py
@@ -12,3 +12,10 @@ class School(models.Model):
 class Grade(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     name = models.CharField(max_length=4)
+
+
+class Subscriber(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    telefon = models.CharField(max_length=200)
+
