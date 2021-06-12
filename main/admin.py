097cc813c069983a14ba416a1af3d159b84c66d1
stefanwacker <wacker@online.de>
2021-06-12 11:52:45 +0200

    Model Subscription generated

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import School, Grade, Subscriber
+from .models import School, Grade, Subscriber, Subscription
 
 # Register your models here.
 admin.site.register(School)
 admin.site.register(Grade)
 admin.site.register(Subscriber)
+admin.site.register(Subscription)
--- main/models.py
+++ main/models.py
@@ -22,3 +22,10 @@ class Subscriber(models.Model):
     email = models.CharField(max_length=200)
     telefon = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
+class Subscription(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
