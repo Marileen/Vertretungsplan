654e7048cfcd0379d9428cbbc170bb6598b38bc4
stefanwacker <wacker@online.de>
2021-06-15 23:06:09 +0200

    added email-settings for gmail-account

--- Vertretungsplan/settings.py
+++ Vertretungsplan/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#For email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'oos2021.thl@gmail.com'
+#Must generate specific password for your app in [gmail settings][1]
+EMAIL_HOST_PASSWORD = 'enzwrdjnrrumyiwe'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
